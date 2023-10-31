3283e2702c1f14c697d633a8fe560b1cfc78576e
VitaliiBondarenko5749 <bvetal2@gmail.com>
2023-10-31 23:53:23 +0200

    Практична робота №24

--- Practice-work-24/SpringBootBootstrap/src/main/java/org/o7planning/sbbootstrap/config/WebMvcConfig.java
+++ Practice-work-24/SpringBootBootstrap/src/main/java/org/o7planning/sbbootstrap/config/WebMvcConfig.java
@@ -1,9 +1,7 @@
 package org.o7planning.sbbootstrap.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 @Override
--- Practice-work-24/SpringBootBootstrap/src/main/java/org/o7planning/sbbootstrap/controller/HelloWorldController.java
+++ Practice-work-24/SpringBootBootstrap/src/main/java/org/o7planning/sbbootstrap/controller/HelloWorldController.java
@@ -1,9 +1,7 @@
 package org.o7planning.sbbootstrap.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class HelloWorldController {
 @RequestMapping("/")
