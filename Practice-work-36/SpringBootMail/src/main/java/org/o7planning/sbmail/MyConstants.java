43cdad14fe15714a8bd772464694b1a5c04e1faa
VitaliiBondarenko5749 <bvetal2@gmail.com>
2023-11-03 19:48:26 +0200

    Практична робота №36

---
+++ Practice-work-36/SpringBootMail/src/main/java/org/o7planning/sbmail/MyConstants.java
@@ -0,0 +1,14 @@
+package org.o7planning.sbmail;
+
+public class MyConstants {
+
+  // Replace with your email here: 
+  public static final String MY_EMAIL = "bvetal2@gmail.com";
+
+  // Replace password!!
+  public static final String MY_PASSWORD = "password";
+
+  // And receiver!
+  public static final String FRIEND_EMAIL = "bvetal2@gmail.com";
+
+}
